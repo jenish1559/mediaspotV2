92484c22f22424a86758ece90390037965eabfb0
JENISH PATEL <72336473+jenish1559@users.noreply.github.com>
2024-07-26 20:51:40 +0530

    modify navbar

---
+++ app/dashboard/_components/mainnav.jsx
@@ -0,0 +1,36 @@
+"use client"
+import React from 'react'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { useParams, usePathname } from 'next/navigation'
+
+  
+
+function MainNav({className}) {
+    const pathname = usePathname();
+    const params = useParams();
+    
+    const routes = [
+        {
+            href : `/dashboard/${params.storeid}/settings`,
+            label: "Settings",
+            active: pathname == `/dashboard/${params.storeid}/settings`
+        }
+    ];
+
+  return (
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6",className)}>
+        {routes.map((route) => (
+            <Link key={route.href} 
+                  href={route.href}
+                  className={cn("tex-sm font-medium transition-colors hover:text-primary ",
+                    route.active ? "text-black dark:text-white" : "text-muted-foreground"
+                  )} >
+            {route.label}
+            </Link>
+        ))}
+    </nav>
+  )
+}
+
+export default MainNav
--- app/dashboard/_components/navbar.jsx
+++ app/dashboard/_components/navbar.jsx
@@ -1,20 +1,34 @@
+import { UserButton } from '@/components/auth/user-button'
 import React from 'react'
+import MainNav from '@/app/dashboard/_components/mainnav'
+import StoreSwitcher from '@/app/dashboard/_components/store-switcher'
+import { auth } from '@/auth'
+import { redirect } from 'next/navigation'
+import { db } from '@/lib/db'
 
-const Navbar = () => {
+const Navbar = async () => {
+  const user = await auth();
+  if(!user){
+    redirect("/auth/login")
+  }
+  const userId = user.id;
+
+  const stores = await db.store.findMany({
+    where : {
+      userId
+    }
+  })
+
   return (
     <div className="border-b">
-        <div className="flex h-16 items-center px-4">
-            <div>
-                Store Swicher
-            </div>
-            <div>
-             routes
-            </div>
-<div>
-    
-</div>
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton />
         </div>
-        
+      </div>
+
     </div>
   )
 }
